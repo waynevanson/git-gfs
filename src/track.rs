cced46f84ece0c6bac1b8783e8dfebfc66563147
Wayne Van Son <waynevanson@gmail.com>
2024-12-06 08:45:44 +1100

    test: add track pattern test

--- src/track.rs
+++ src/track.rs
@@ -12,3 +12,23 @@ fn pattern_from_str(str: &str) -> Result<Pattern> {
     let pattern = parse(str).ok_or_else(|| anyhow!("Expected a pattern but received '{}'", str))?;
     Ok(pattern)
 }
+
+#[cfg(test)]
+mod test {
+    use super::pattern_from_str;
+    use gix::glob::{pattern::Mode, Pattern};
+
+    #[test]
+    fn parse_pattern_from_str() {
+        let pattern = "*.txt";
+        let result = pattern_from_str(pattern).unwrap();
+
+        let expected = Pattern {
+            text: pattern.into(),
+            first_wildcard_pos: Some(0),
+            mode: Mode::NO_SUB_DIR | Mode::ENDS_WITH,
+        };
+
+        assert_eq!(result, expected);
+    }
+}
