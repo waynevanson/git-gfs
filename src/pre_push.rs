9ea712d2bff801718290a0e9003f06eb76b4a997
Wayne Van Son <waynevanson@gmail.com>
2024-12-12 09:21:35 +1100

    chore: add some prepush work

--- src/pre_push.rs
+++ src/pre_push.rs
@@ -1,5 +1,9 @@
-use anyhow::Result;
-use gix::Repository;
+use anyhow::{anyhow, Result};
+use gix::{
+    bstr::{BStr, BString, ByteSlice},
+    Id, Repository,
+};
+use itertools::Itertools;
 use std::io::{stdin, Read};
 
 // get all the commits within the range (from stdin)
@@ -9,9 +13,38 @@ use std::io::{stdin, Read};
 // get the reference hash
 // push `/refs/gfs/<hash>`, one hash at a time.
 pub fn pre_push(repo: &mut Repository) -> Result<()> {
-    // hash is 64 chars, surely 4 of these will be big enough.
-    let mut buf = Vec::with_capacity(64 * 4);
-    stdin().read_to_end(&mut buf)?;
+    // get a list of commits that we're going to push.
+    // rev_walk?
+    let bstr = bstring_from_stdin()?;
+
+    let tips = spec_tips_from_bstr(repo, bstr.as_ref())?;
+
+    let _walk = repo.rev_walk(tips).first_parent_only().all()?;
 
     todo!("pre-push hook not implemented");
+}
+
+fn bstring_from_stdin() -> Result<BString> {
+    let mut buf = Vec::with_capacity(64 * 8);
+    stdin().read_to_end(&mut buf)?;
+    Ok(buf.into())
+}
+
+fn spec_tips_from_bstr<'repo>(repo: &'repo Repository, bstr: &BStr) -> Result<[Id<'repo>; 2]> {
+    let (from, to) = ids_from_bstr(bstr)?;
+
+    let from = repo.rev_parse_single(from)?;
+    let to = repo.rev_parse_single(to)?;
+
+    Ok([from, to])
+}
+
+fn ids_from_bstr(bstr: &BStr) -> Result<(&BStr, &BStr)> {
+    let (_, from, _, to): (_, _, _, _) = bstr
+        .split(|byte| byte == &b' ')
+        .map(|byte| byte.as_bstr())
+        .collect_tuple()
+        .ok_or_else(|| anyhow!("Expected to find a string with 3 spaces in between"))?;
+
+    Ok((from, to))
 }
